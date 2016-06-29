647243445b3d13ad867ac045fa3615d8b3d17128
Jose Chen <cqxcool@163.com>
2016-06-29 16:19:09 +0800

    YoutubeDemo
    YoutubeDemo

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios,'8.0'
+pod 'youtube-ios-player-helper', '~> 0.1.6'
+#pod 'GoogleAPIClient', '~> 1.0.4'
+#pod 'gtm-oauth2', '~> 1.0.126'
---
+++ YoutubeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FacebookDemo
+//
+//  Created by Jose Chen on 16/5/30.
+//  Copyright © 2016年 Jose Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
